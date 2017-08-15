709ef7b1f814a9d6780738ddf10081f8546a9804
abhisit sungnark <abhisit2537@hotmail.com>
2017-08-15 18:20:52 +0700

    tt

--- modules/products/server/routes/products.server.routes.js
+++ modules/products/server/routes/products.server.routes.js
@@ -8,16 +8,16 @@ var productsPolicy = require('../policies/products.server.policy'),
 
 module.exports = function (app) {
   // Products Routes
-  app.route('/api/products').all(productsPolicy.isAllowed)
+  app.route('/api/products')//.all(productsPolicy.isAllowed)
     .get(products.list)
     .post(products.create);
 
-  app.route('/api/products/:productId').all(productsPolicy.isAllowed)
+  app.route('/api/products/:productId')//.all(productsPolicy.isAllowed)
     .get(products.read)
     .put(products.update)
     .delete(products.delete);
 
-  app.route('/api/productbyshopid/:shopId').all(productsPolicy.isAllowed)
+  app.route('/api/productbyshopid/:shopId')//.all(productsPolicy.isAllowed)
     .get(products.productbyshopid);
   // Finish by binding the Product middleware
   app.param('productId', products.productByID);
