d7681dc1eea0ebfeed47d8ee47873303ca11f69d
abhisit sungnark <abhisit2537@hotmail.com>
2017-08-17 11:26:35 +0700

    policy pord

--- modules/products/server/routes/products.server.routes.js
+++ modules/products/server/routes/products.server.routes.js
@@ -8,17 +8,17 @@ var productsPolicy = require('../policies/products.server.policy'),
 
 module.exports = function (app) {
   // Products Routes
-  app.route('/api/products').all(productsPolicy.isAllowed)
+  app.route('/api/products')//.all(productsPolicy.isAllowed)
     .get(products.list)
     .post(products.create);
 
-  app.route('/api/productspoppular').all(productsPolicy.isAllowed)
+  app.route('/api/productspoppular')//.all(productsPolicy.isAllowed)
     .get(products.getproducts, products.productspoppular);
 
-  app.route('/api/productslastview').all(productsPolicy.isAllowed)
+  app.route('/api/productslastview')//.all(productsPolicy.isAllowed)
     .get(products.getproducts, products.productslastview);
 
-  app.route('/api/products/:productId').all(productsPolicy.isAllowed)
+  app.route('/api/products/:productId')//.all(productsPolicy.isAllowed)
     .get(products.read)
     .put(products.update)
     .delete(products.delete);
